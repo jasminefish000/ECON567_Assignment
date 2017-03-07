af1b46adf7556e1741a536d834df3dd77a429984
jasminefish000 <haojasmine@gmail.com>
2017-03-07 00:04:07 -0800

    Finished Assignment2 1-6

--- HW2.R
+++
@@ -1,6 +0,0 @@
-library(hdm)
-data(BLP)
-tab2 <- c(3.393, 6.711, 8.728, 13.074, 68.597)
-stopifnot(all(abs(quantile(BLP$price, c(0,0.25,0.5,0.75,1)) - tab2 +
-                    11.761)<0.005))
-BLP$price <- BLP$price + 11.761
